23fa5ddd439004503e84acee09ef45c454b14d2c
Alpha-Cyberus <ammonobrien@gmail.com>
2021-08-10 09:45:28 +0200

    Changelog v0.0.1a - define project wireframe and workflow

---
+++ mx_mul.py
@@ -0,0 +1,23 @@
+#title and declarations
+
+#main menu
+
+#define and size of matrices
+
+#confirm if matrices can be multiplied
+
+	#if no return to menu
+
+	#if yes carry on
+
+#begin multiplication
+
+	#iterate through matrices
+
+		#call multiplication module
+
+	#output to result matrix
+
+#display results in eof menu
+
+#multiplication module for single row
