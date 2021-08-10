a73b59b04fcb8a585d56698e83da8bb0f58ad8a2
Alpha-Cyberus <ammonobrien@gmail.com>
2021-08-10 19:34:21 +0200

    Changelog v0.0.2a - matrix creation is dynamic from matrix class - user can define size of matrices - feedback to confirm matrix dimensions

--- mx_mul.py
+++ mx_mul.py
@@ -1,23 +1,88 @@
-#title and declarations
+# title and declarations
+class Matrix:
+    def __init__(self, r, c):
+        self.row = r
+        self.col = c
+        contents = []
 
-#main menu
+    def size(self):
+        # return '{}x{}'.format(self.row, self.col)
+        return '[%dx%d]' % (self.row, self.col)
 
-#define and size of matrices
 
-#confirm if matrices can be multiplied
+mx_list = []
+# main menu
 
-	#if no return to menu
+# define size of matrices
 
-	#if yes carry on
 
-#begin multiplication
+def mx_size_set(dim):
 
-	#iterate through matrices
+    print("How many "+dim+"?")
+    while True:
+        try:
+            n = int(input(""))
+        except ValueError:
+            print("That's not a number. Input a whole number.")
+            continue
+        if n == 0:
+            print("Number must not be 0.")
+            continue
+        else:
+            break
+    return n
 
-		#call multiplication module
 
-	#output to result matrix
+def mx_size_confirm(mxnum, r, c):
 
-#display results in eof menu
+    print("Matrix %s size is [%sx%s] \r\nIs this correct?" % (mxnum, r, c))
+    while True:
+        yn = input("[y/n]")
+        if yn.lower() in ['y', 'yes']:
+            print("confirmed yes")
+            break
+        elif yn.lower() in ['n', 'no']:
+            print("confirmed no")
+            mx_size(mxnum)
+            break
+        else:
+            continue
 
-#multiplication module for single row
+
+def mx_size(mxnum):
+    print("Configure Matrix %s" % mxnum)
+    r = mx_size_set("rows")
+    c = mx_size_set("columns")
+    mx_size_confirm(mxnum, r, c)
+    mx_list.append(Matrix(r, c))
+
+
+def define_matrices():
+    i = int(1)
+    while i <= 2:
+        mx_size(i)
+        i = i+1
+    print("matrix 1 = "+mx_list[0].size())
+    print("matrix 2 = "+mx_list[1].size())
+
+
+define_matrices()
+
+#   confirm if matrices can be multiplied
+
+#       if no return to menu
+
+#       if yes carry on
+
+# begin multiplication
+
+#   iterate through matrices
+
+#   call multiplication module
+
+#   output to result matrix
+
+# display results in eof menu
+
+# multiplication module for single row
+# multiplication module for single row
