4950ef4023e48d2f03214d7869765949853523fb
Alpha-Cyberus <ammonobrien@gmail.com>
2021-08-11 10:26:02 +0200

    Changelog v0.0.3a - add matrix compatibility testing - fix negative numbers for matrix size - improve comment organization - improve fully dynamic matrix instantiation and referencing

--- mx_mul.py
+++ mx_mul.py
@@ -1,23 +1,24 @@
-# title and declarations
+#                                       Matrix Multiplier by Alpha_Cyberus (Luffy O'Brien)
 class Matrix:
     def __init__(self, r, c):
         self.row = r
         self.col = c
-        contents = []
+        # values = []
 
     def size(self):
-        # return '{}x{}'.format(self.row, self.col)
         return '[%dx%d]' % (self.row, self.col)
 
 
 mx_list = []
-# main menu
-
-# define size of matrices
+mxcomp = False
 
 
-def mx_size_set(dim):
+def main():
+    generate_matrices()
+    compat_test()
 
+
+def mx_size_set(dim):                   # define size of single matrix dimension
     print("How many "+dim+"?")
     while True:
         try:
@@ -25,17 +26,16 @@ def mx_size_set(dim):
         except ValueError:
             print("That's not a number. Input a whole number.")
             continue
-        if n == 0:
-            print("Number must not be 0.")
+        if n <= 0:
+            print("Number must at least 1.")
             continue
         else:
             break
     return n
 
 
-def mx_size_confirm(mxnum, r, c):
-
-    print("Matrix %s size is [%sx%s] \r\nIs this correct?" % (mxnum, r, c))
+def mx_size_confirm(mxnum, r, c):       # confirm size of single matrix dimension
+    print("Matrix %s's size is [%sx%s] \r\nIs this correct?" % (mxnum, r, c))
     while True:
         yn = input("[y/n]")
         if yn.lower() in ['y', 'yes']:
@@ -49,40 +49,44 @@ def mx_size_confirm(mxnum, r, c):
             continue
 
 
-def mx_size(mxnum):
-    print("Configure Matrix %s" % mxnum)
+def mx_size(mxnum):                     # instantiate empty matrix, add to mx_list
+    print("Configure Matrix %s's dimensions" % mxnum)
     r = mx_size_set("rows")
     c = mx_size_set("columns")
     mx_size_confirm(mxnum, r, c)
     mx_list.append(Matrix(r, c))
 
 
-def define_matrices():
+def generate_matrices():                # generate n matrices for multiplication
     i = int(1)
-    while i <= 2:
+    while i <= 2:                       # mow many matrices to make
         mx_size(i)
         i = i+1
-    print("matrix 1 = "+mx_list[0].size())
-    print("matrix 2 = "+mx_list[1].size())
+    for m in range(len(mx_list)):
+        print("matrix " + str(m + 1) + " = " + mx_list[m].size())
 
 
-define_matrices()
+def compat_test():                      # test matrix compatibility for multiplication
+    global mxcomp
+    if mxcomp:
+        mxcomp = False
+    if mx_list[0].col == mx_list[1].row:
+        print("compatible!")
+        mxcomp = True
+    else:
+        print("These matrices are incompatible and cannot be multiplied. Matrix 1's rows must be equal to Matrix 2's columns.")
 
-#   confirm if matrices can be multiplied
 
-#       if no return to menu
+main()
 
-#       if yes carry on
+#                                       populate matrices with values
+#                                       begin multiplication
+#                                       iterate through matrices
+#                                       output to result matrix
+#                                       display results
 
-# begin multiplication
+#                                       MATRIX CALCULATION
+#                                       compatibility = MX1.col = MX2.rows
+#                                       result = MX1.row x MX2.col
 
-#   iterate through matrices
-
-#   call multiplication module
-
-#   output to result matrix
-
-# display results in eof menu
-
-# multiplication module for single row
-# multiplication module for single row
+# Changelog v0.0.3a - add matrix compatibility testing - fix negative numbers for matrix size - improve comment organization - improve fully dynamic matrix instantiation and referencing
