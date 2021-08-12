e5f6d5f5f3698a1aebb766bfc8ed59c0aa965330
Alpha-Cyberus <ammonobrien@gmail.com>
2021-08-12 09:04:52 +0200

    Changelog v1.0.0 All required features fully functional, additional features sufficiently reliable, ready for release. added - matrix compatibility testing fully functional improved - better input prompts and feedback

--- mx_mul.py
+++ mx_mul.py
@@ -13,19 +13,33 @@ class Matrix:
             print(self.val[r])
 
 
-mx_list = []
-mx_result = Matrix(0, 0)
-mxcomp = False
+mx_list = []                            # collection of every matrix user will generate
+mx_result = Matrix(0, 0)                # matrix to hold results
 
 
 def main():
-    for i in range(2):                 # mow many matrices to make
-        mx_define(i + 1)
+    mx_count = 2                        # controls how many matrices should be generated
+    compatible = False
+    while not compatible:
+        for i in range(mx_count):
+            mx_define(i + 1)
+        compatible = compat_test()
     mx_populate()
     mx_confirm()
     mx_multiply()
     print("The product of these matrices is:")
     mx_result.printval()
+
+
+def mx_define(mxnum):                   # instantiate empty matrix, add to mx_list
+    print("Define Matrix %s's dimensions" % mxnum)
+    correct = False
+    while not correct:
+        r = mx_size_set("rows")
+        c = mx_size_set("columns")
+        correct = mx_size_confirm(mxnum, r, c)
+    mx_list.append(Matrix(r, c))
+    print("Matrix %s saved" % mxnum)
 
 
 def mx_size_set(dim):                   # define size of single matrix dimension
@@ -44,49 +58,34 @@ def mx_size_set(dim):                   # define size of single matrix dimension
     return n
 
 
-def mx_size_confirm(mxnum, r, c):       # prompt user to confirm matrix dimension is correct
-    print("Matrix %s's size is [%sx%s] \r\nIs this correct?" % (mxnum, r, c))
+def mx_size_confirm(mxnum, r, c):       # prompt user to confirm matrix dimensions are correct
+    print("Matrix %s's dimensions are [%sx%s]. \r\nIs this correct?" % (mxnum, r, c))
     while True:
         yn = input("[y/n]")
         if yn.lower() in ['y', 'yes']:
-            print("confirmed yes")
-            break
+            return True
         elif yn.lower() in ['n', 'no']:
-            print("confirmed no")
-            mx_define(mxnum)
             break
         else:
             continue
 
 
-def mx_define(mxnum):                     # instantiate empty matrix, add to mx_list
-    print("Define Matrix %s's dimensions" % mxnum)
-    r = mx_size_set("rows")
-    c = mx_size_set("columns")
-    # mx_size_confirm(mxnum, r, c)
-    # compat_test()
-    mx_list.append(Matrix(r, c))
-
-
-def compat_test():                      # test matrix compatibility for multiplication
-    global mxcomp
-    if mxcomp:
-        mxcomp = False
+def compat_test():                      # test matrix compatibility
     if mx_list[0].col == mx_list[1].row:
-        print("compatible!")
-        mxcomp = True
+        return True
     else:
-        print("These matrices are incompatible and cannot be multiplied. Matrix 1's columns must be equal to Matrix 2's rows.")
+        for i in range(len(mx_list)):
+            mx_list.pop()
+        print("These matrices are incompatible and have been deleted. Matrix 1's columns Matrix 2's rows must be equal values.\r\nExample: [?x3]x[3x?]")
 
 
 def mx_populate():                      # allow user to populate matrices with values
-    # loop all matrices
     print("Input values for each matrix one at a time. Values can be any whole number.")
-    for imat in range(len(mx_list)):                # loop each matrix
+    for imat in range(len(mx_list)):                # for each matrix
         print("Matrix " + str(imat + 1) + ":")
-        for irow in range(mx_list[imat].row):       # loop each row
+        for irow in range(mx_list[imat].row):       # for each row
             mx_list[imat].val.append([])
-            for icol in range(mx_list[imat].col):   # loop each column
+            for icol in range(mx_list[imat].col):   # for every column within each row
                 print("Element " + str(irow + 1) + "," + str(icol + 1))
                 while True:
                     try:
@@ -100,7 +99,7 @@ def mx_populate():                      # allow user to populate matrices with v
 
 
 def mx_confirm():                       # display values in all matrices
-    print("Values of each matrix are:")
+    print("Each matrix's values are:")
     for imat in range(len(mx_list)):
         print("Matrix " + str(imat + 1) + ":")
         mx_list[imat].printval()
@@ -117,4 +116,3 @@ def mx_multiply():                      # multiply matrices together
 
 
 main()
-# Changelog v0.2.0a - added - matrix multiplication - improved - prompts
